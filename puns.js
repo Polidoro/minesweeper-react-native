9ed08ecf59ce995f530daf1f61e5c343e6fb202c
Vincent Polidoro <polidoro@gmail.com>
2016-12-14 17:24:41 -0500

    Adding larger range of baords

--- puns.js
+++ puns.js
@@ -1,14 +1,14 @@
 export default {
   easy: [
     {
-      boardCols: 10,
+      boardCols: 6,
       boardRows: 6,
       question: 'How did I escape Iraq?',
       answer: 'Iran',
     },
     {
-      boardCols: 10,
-      boardRows: 6,
+      boardCols: 8,
+      boardRows: 8,
       question: 'Which country has the most birds?',
       answer: 'Portugull',
     },
@@ -16,33 +16,33 @@ export default {
   medium: [
     {
       boardCols: 10,
-      boardRows: 6,
+      boardRows: 12,
       question: 'What does a house wear?',
       answer: 'A dress',
     },
     {
       boardCols: 10,
-      boardRows: 6,
+      boardRows: 12,
       question: 'What do you do when chemists die?',
       answer: 'Barrium',
     },
   ],
   hard: [
     {
-      boardCols: 10,
-      boardRows: 6,
+      boardCols: 11,
+      boardRows: 10,
       question: 'What do you call cheese that doesn\'t belong to you?',
       answer: 'Nacho Cheese',
     },
     {
-      boardCols: 10,
-      boardRows: 6,
+      boardCols: 11,
+      boardRows: 13,
       question: 'What does it sound like when a piano falls down a mineshaft?',
       answer: 'A Flat Minor',
     },
     {
-      boardCols: 10,
-      boardRows: 6,
+      boardCols: 11,
+      boardRows: 13,
       question: 'Why couldn\'t the bicycle go for a ride?',
       answer: 'It was two tired',
     },
